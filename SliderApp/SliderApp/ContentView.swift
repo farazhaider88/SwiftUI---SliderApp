f1240bfabc031e88daa910314a001790c61ccd1e
farazhaider88 <farazhaider88@yahoo.com>
2020-06-11 11:32:01 +0500

    Initial Commit

---
+++ SliderApp/SliderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderApp
+//
+//  Created by Faraz Haider on 11/06/2020.
+//  Copyright © 2020 Etisalat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
